75b25beb4da05cbc33749c60e8f328c054ab23dd
Tanin Rojanapiansatith <sp_land@hotmail.com>
2019-03-13 19:52:14 +0000

    First commit, got an empty tableview

---
+++ Checklist/ChecklistViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Tanin on 13/03/2019.
+//  Copyright © 2019 landtanin. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+    return cell
+  }
+  
+}
+
