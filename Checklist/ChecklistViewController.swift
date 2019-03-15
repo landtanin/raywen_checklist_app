0193bb21a03d5a64866435ccbcd703b85d582f29
Tanin Rojanapiansatith <sp_land@hotmail.com>
2019-03-15 10:14:48 +0000

    Add 1000 rows to the tableView

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -16,18 +16,29 @@ class ChecklistViewController: UITableViewController {
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 1000
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
     
     if let label = cell.viewWithTag(1000) as? UILabel {
-      if indexPath.row == 0 {
-        label.text = "Run a marathon"
-      } else {
-        label.text = "Sleep"
+      
+      switch(indexPath.row % 5) {
+      case 0:
+        label.text = "Take a job"
+      case 1:
+        label.text = "Watch a movie"
+      case 2:
+        label.text = "Code an app"
+      case 3:
+        label.text = "Walk the dog"
+      case 4:
+        label.text = "Study design patterns"
+      default:
+        label.text = "default case"
       }
+      
     }
     
     return cell
