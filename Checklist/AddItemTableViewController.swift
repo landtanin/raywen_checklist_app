4b5fb24e248c36223da4fb91229ca567d7583194
Tanin Rojanapiansatith <sp_land@hotmail.com>
2019-03-19 18:35:07 +0000

    Play around with textField.firstResponder to show and dismiss keyboard

--- Checklist/AddItemTableViewController.swift
+++ Checklist/AddItemTableViewController.swift
@@ -10,17 +10,39 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
   
+  @IBOutlet weak var textField: UITextField!
+  
   @IBAction func cancelBtn(_ sender: Any) {
     navigationController?.popViewController(animated: true)
   }
   
   @IBAction func addBtn(_ sender: Any) {
-    
+    print("Contents of the text field \(textField.text)")
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.largeTitleDisplayMode = .never
+    textField.delegate = self
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    // This simulate a tap on textField which brings up the keyboard
+    textField.becomeFirstResponder()
+  }
+  
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
   }
     
+}
+
+extension AddItemTableViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // This will dismiss the keyboard
+    textField.resignFirstResponder()
+    return false
+  }
+  
 }
